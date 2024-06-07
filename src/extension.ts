1e5ee2f51403620387d0c524a9e5d9a66972eb67
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-06-07 19:51:35 +0200

    feat: Add CSV recording and SRT generation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/semi */
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { on } from 'events'
 import * as fs from 'fs'
 import * as util from 'util'
@@ -30,7 +28,11 @@ enum ChangeType {
 	TAB = 'tab',
 }
 
-function generateFileName() {
+/**
+ * Generates a unique file name based on the current date and time.
+ * @returns A string representing the generated file name.
+ */
+function generateFileName(): string {
 	const date = new Date()
 	return `vs-code-recorder-${date.getFullYear()}_${date.getMonth()}_${date.getDate()}-${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}.${date.getMilliseconds()}`
 }
@@ -50,14 +52,18 @@ function buildCsvRow(
 	rangeLength: number,
 	text: string,
 	type: string = ChangeType.CONTENT
-) {
+): string {
 	const time = new Date().getTime() - startDateTime.getTime()
 	return `${sequence},${time},"${getEditorFileName()}",${rangeOffset},${rangeLength},"${escapeString(
 		text
 	)}",${type}\n`
 }
 
-function getEditorFileName() {
+/**
+ * Gets the relative path of the active text editor's file.
+ * @returns A string representing the relative path of the active text editor's file.
+ */
+function getEditorFileName(): string {
 	return vscode.workspace.asRelativePath(vscode.window.activeTextEditor?.document.fileName || '')
 }
 
@@ -67,22 +73,19 @@ const onChangeSubscription = vscode.workspace.onDidChangeTextDocument(event => {
 	}
 	const editor = vscode.window.activeTextEditor
 	if (editor && event.document === editor.document) {
-		const editor = vscode.window.activeTextEditor
-		if (editor && event.document === editor.document) {
-			// console.log('Documento modificato:', event.document.uri.toString())
-			// console.log('date diff:', new Date().getTime() - startDateTime.getTime())
-
-			event.contentChanges.forEach(change => {
-				sequence++
-
-				addToFileQueue(buildCsvRow(sequence, change.rangeOffset, change.rangeLength, change.text))
-				appendToFile()
-			})
-		}
+		event.contentChanges.forEach(change => {
+			sequence++
+			addToFileQueue(buildCsvRow(sequence, change.rangeOffset, change.rangeLength, change.text))
+			appendToFile()
+		})
 	}
 })
 
-async function startRecording(context: vscode.ExtensionContext) {
+/**
+ * Starts the recording process and initializes necessary variables.
+ * @param context - The extension context.
+ */
+async function startRecording(context: vscode.ExtensionContext): Promise<void> {
 	if (recording) {
 		notificationWithProgress('Already recording')
 		return
@@ -97,9 +100,7 @@ async function startRecording(context: vscode.ExtensionContext) {
 	}, 1000)
 	notificationWithProgress('Recording started')
 
-	//get editor current text
 	const editorText = vscode.window.activeTextEditor?.document.getText()
-	//get editor file name
 	const editorFileName = vscode.workspace.asRelativePath(
 		vscode.window.activeTextEditor?.document.fileName || ''
 	)
@@ -113,15 +114,18 @@ async function startRecording(context: vscode.ExtensionContext) {
 	updateStatusBarItem()
 }
 
-function stopRecording(context: vscode.ExtensionContext) {
+/**
+ * Stops the recording process and finalizes the recording data.
+ * @param context - The extension context.
+ */
+function stopRecording(context: vscode.ExtensionContext): void {
 	if (!recording) {
 		notificationWithProgress('Not recording')
 		return
 	}
 	recording = false
-	clearInterval(intervalId as NodeJS.Timeout)
+	clearInterval(intervalId)
 	timer = 0
-	// Remove the subscription from the array
 	const index = context.subscriptions.indexOf(onChangeSubscription)
 	if (index !== -1) {
 		context.subscriptions.splice(index, 1)
@@ -132,6 +136,9 @@ function stopRecording(context: vscode.ExtensionContext) {
 	updateStatusBarItem()
 }
 
+/**
+ * Updates the status bar item with the current recording status and time.
+ */
 function updateStatusBarItem(): void {
 	if (recording) {
 		myStatusBarItem.text = `$(debug-stop) ${formatDisplayTime(timer)}`
@@ -142,16 +149,19 @@ function updateStatusBarItem(): void {
 	}
 }
 
-function notificationWithProgress(title: string) {
+/**
+ * Displays a notification with progress in VS Code.
+ * @param title - The title of the notification.
+ */
+function notificationWithProgress(title: string): void {
 	vscode.window.withProgress(
 		{
 			location: vscode.ProgressLocation.Notification,
 			title: title,
 			cancellable: false,
 		},
-
-		(progress, token) => {
-			return new Promise<void>((resolve, reject) => {
+		progress => {
+			return new Promise<void>(resolve => {
 				const times = 1.5 * 1000
 				const timeout = 50
 				const increment = (100 / times) * timeout
@@ -168,6 +178,11 @@ function notificationWithProgress(title: string) {
 	)
 }
 
+/**
+ * Formats a time value in seconds to a display string.
+ * @param seconds - The number of seconds.
+ * @returns A string representing the formatted time.
+ */
 function formatDisplayTime(seconds: number): string {
 	const hours = Math.floor(seconds / 3600)
 	const minutes = Math.floor((seconds % 3600) / 60)
@@ -186,6 +201,11 @@ function formatDisplayTime(seconds: number): string {
 	return timeString
 }
 
+/**
+ * Formats a time value in milliseconds to an SRT time string.
+ * @param milliseconds - The number of milliseconds.
+ * @returns A string representing the formatted SRT time.
+ */
 function formatSrtTime(milliseconds: number): string {
 	const seconds = Math.floor(milliseconds / 1000)
 	const hours = Math.floor(seconds / 3600)
@@ -193,26 +213,32 @@ function formatSrtTime(milliseconds: number): string {
 	const remainingSeconds = seconds % 60
 	const remainingMilliseconds = milliseconds % 1000
 
-	let timeString = ''
-
-	timeString += `${hours.toString().padStart(2, '0')}:`
-
-	timeString += `${minutes.toString().padStart(2, '0')}:${remainingSeconds
+	return `${hours.toString().padStart(2, '0')}:${minutes
 		.toString()
-		.padStart(2, '0')},${remainingMilliseconds.toString().padStart(3, '0')}`
-
-	return timeString
+		.padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')},${remainingMilliseconds
+		.toString()
+		.padStart(3, '0')}`
 }
 
 const appendFile = util.promisify(fs.appendFile)
 
-async function appendToFile() {
+/**
+ * Appends data from the file queue to the appropriate file in the workspace.
+ */
+async function appendToFile(): Promise<void> {
 	const workspaceFolders = vscode.workspace.workspaceFolders
 
 	if (workspaceFolders) {
 		const workspacePath = workspaceFolders[0].uri.fsPath
+		const recorderPath = path.join(workspacePath, '/vs-code-recorder/')
+
+		// Create the directory if it does not exist
+		if (!fs.existsSync(recorderPath)) {
+			fs.mkdirSync(recorderPath, { recursive: true })
+		}
+
 		while (fileQueue.length) {
-			const filePath = path.join(workspacePath, fileQueue[0].name)
+			const filePath = path.join(recorderPath, fileQueue[0].name)
 			await addToFile(filePath, fileQueue[0].content)
 		}
 	} else {
@@ -220,47 +246,58 @@ async function appendToFile() {
 	}
 }
 
-async function addToFile(filePath: string, text: string) {
+/**
+ * Appends text to a file at the specified file path.
+ * @param filePath - The path to the file.
+ * @param text - The text to append.
+ */
+async function addToFile(filePath: string, text: string): Promise<void> {
 	try {
 		await appendFile(filePath, text)
 		fileQueue.shift()
-		console.log('Successfully appended to file')
 	} catch (err) {
 		console.error('Failed to append to file:', err)
 	}
 }
 
-function escapeString(editorText: string | undefined) {
+/**
+ * Escapes special characters in a string for CSV compatibility.
+ * @param editorText - The text to escape.
+ * @returns A string with escaped characters.
+ */
+function escapeString(editorText: string | undefined): string {
 	if (editorText === undefined) {
 		return ''
 	}
-	// Replace double quotes with escaped double quotes
-	const escapedText = editorText
+	return editorText
 		.replace(/"/g, '""')
 		.replace(/\r\n/g, '\\r\\n')
 		.replace(/\n/g, '\\n')
 		.replace(/\r/g, '\\r')
 		.replace(/\t/g, '\\t')
-	return escapedText
 }
 
 const csvFile: string[] = []
 
 interface Change {
 	sequence: number
+	file: string
 	startTime: number
 	endTime: number
 	text: string
 }
 
-async function processCsvFile() {
+/**
+ * Processes the CSV file and generates the necessary output files.
+ */
+async function processCsvFile(): Promise<void> {
 	const workspaceFolders = vscode.workspace.workspaceFolders
 	if (!workspaceFolders) {
 		console.error('No workspace folder found')
 		return
 	}
 	const workspacePath = workspaceFolders[0].uri.fsPath
-	const filePath = path.join(workspacePath, fileName + '.csv')
+	const filePath = path.join(workspacePath, '/vs-code-recorder/', fileName + '.csv')
 	const fileStream = fs.createReadStream(filePath)
 	const rl = readline.createInterface({
 		input: fileStream,
@@ -269,9 +306,6 @@ async function processCsvFile() {
 	let i = 0
 	const processedChanges: Change[] = []
 	for await (const line of rl) {
-		if (line === '') {
-		}
-		// Process each line of the CSV file here
 		const lineArr = line.split(/,(?=(?:[^"]*"[^"]*")*[^"]*$)/)
 
 		const sequence = parseInt(lineArr[0])
@@ -284,7 +318,6 @@ async function processCsvFile() {
 		const rangeLength = parseInt(lineArr[4])
 		const text = unescapeString(removeDoubleQuotes(lineArr[5]))
 		const type = lineArr[6]
-		console.log({ sequence, time, file, rangeOffset, rangeLength, text })
 
 		let newText = ''
 		if (type === ChangeType.TAB) {
@@ -294,7 +327,7 @@ async function processCsvFile() {
 			newTextSplit.splice(rangeOffset, rangeLength, text)
 			newText = newTextSplit.join('')
 		}
-		processedChanges.push({ sequence, startTime: time, endTime: 0, text: newText })
+		processedChanges.push({ sequence, file, startTime: time, endTime: 0, text: newText })
 		if (i > 0) {
 			processedChanges[i - 1].endTime = time
 			addToFileQueue(
@@ -302,7 +335,7 @@ async function processCsvFile() {
 					processedChanges[i - 1].sequence,
 					processedChanges[i - 1].startTime,
 					processedChanges[i - 1].endTime,
-					processedChanges[i - 1].text
+					JSON.stringify({ text: processedChanges[i - 1].text, file: processedChanges[i - 1].file })
 				),
 				'srt'
 			)
@@ -315,23 +348,31 @@ async function processCsvFile() {
 			processedChanges[i - 1].sequence,
 			processedChanges[i - 1].startTime,
 			processedChanges[i - 1].endTime,
-			processedChanges[i - 1].text
+			JSON.stringify({ text: processedChanges[i - 1].text, file: processedChanges[i - 1].file })
 		),
 		'srt'
 	)
 
-	console.log('Processed changes:', processedChanges)
 	addToFileQueue(JSON.stringify(processedChanges), 'json')
 	appendToFile()
 	rl.close()
 }
 
-// remove double quotes at start and end of text
-function removeDoubleQuotes(text: string) {
+/**
+ * Removes double quotes at the start and end of a text string.
+ * @param text - The text to process.
+ * @returns A string without surrounding double quotes.
+ */
+function removeDoubleQuotes(text: string): string {
 	return text.replace(/^"(.*)"$/, '$1')
 }
 
-function unescapeString(text: string) {
+/**
+ * Unescapes special characters in a string.
+ * @param text - The text to unescape.
+ * @returns A string with unescaped characters.
+ */
+function unescapeString(text: string): string {
 	return text
 		.replace(/""/g, '"')
 		.replace(/\\r\\n/g, '\r\n')
@@ -340,27 +381,37 @@ function unescapeString(text: string) {
 		.replace(/\\t/g, '\t')
 }
 
-function addSrtLine(sequence: number, start: number, end: number, text: string) {
+/**
+ * Adds a line to the SRT file format.
+ * @param sequence - The sequence number of the change.
+ * @param start - The start time of the change.
+ * @param end - The end time of the change.
+ * @param text - The text of the change.
+ * @returns A string representing a line in the SRT file format.
+ */
+function addSrtLine(sequence: number, start: number, end: number, text: string): string {
 	return `${sequence}\n${formatSrtTime(start)} --> ${formatSrtTime(end)}\n${text}\n\n`
 }
 
-function addToFileQueue(content: string, fileExtension: string = 'csv') {
+/**
+ * Adds content to the file queue.
+ * @param content - The content to add.
+ * @param fileExtension - The file extension (optional, defaults to 'csv').
+ */
+function addToFileQueue(content: string, fileExtension: string = 'csv'): void {
 	fileQueue.push({
 		name: fileName + '.' + fileExtension,
 		content: content,
 	})
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+/**
+ * Activates the VS Code extension and sets up commands and event listeners.
+ * @param context - The extension context.
+ */
+export function activate(context: vscode.ExtensionContext): void {
 	console.log('Activating VS Code Recorder')
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	context.subscriptions.push(
 		vscode.commands.registerCommand(startRecordingCommand, () => {
 			startRecording(context)
@@ -372,11 +423,9 @@ export function activate(context: vscode.ExtensionContext) {
 			stopRecording(context)
 		})
 	)
-	context.subscriptions.push(vscode.commands.registerCommand('vs-code-recorder.init', () => {}))
 
 	vscode.window.onDidChangeActiveTextEditor(editor => {
 		if (editor && recording) {
-			// Code to handle changes in the focused tab
 			const fileName = editor.document.fileName
 			const editorText = vscode.window.activeTextEditor?.document.getText()
 			sequence++
@@ -391,7 +440,9 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(myStatusBarItem)
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {
+/**
+ * Deactivates the VS Code extension.
+ */
+export function deactivate(): void {
 	console.log('Deactivating VS Code Recorder')
 }
